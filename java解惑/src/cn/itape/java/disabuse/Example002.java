722a044b28d751366ea06b1986dc598a7bc01267
imu2008 <panhuawenmail@gmail.com>
2014-12-14 22:16:18 +0800

    init

---
+++ java解惑/src/cn/itape/java/disabuse/Example002.java
@@ -0,0 +1,8 @@
+package cn.itape.java.disabuse;
+
+public class Example002 {
+	public static void main(String args[]) {
+		System.out.println("println:( 2.00 - 1.10) = " + (2.00 - 1.10));
+		System.out.printf("printf:( 2.00 - 1.10) = %.2f%n", 2.00 - 1.10);
+	}
+}
