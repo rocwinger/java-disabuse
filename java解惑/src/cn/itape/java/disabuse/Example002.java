6ff996d9123a42e5880e23c19ccd07f9bf74bc20
imu2008 <panhuawenmail@gmail.com>
2014-12-15 21:35:22 +0800

    使用double进行小数运算

--- java解惑/src/cn/itape/java/disabuse/Example002.java
+++ java解惑/src/cn/itape/java/disabuse/Example002.java
@@ -1,8 +1,17 @@
 package cn.itape.java.disabuse;
 
+import java.math.BigDecimal;
+
+/**
+ * 具体请参考 http://imu2008.blog.51cto.com/3844842/1590229
+ * @author winger
+ *
+ */
 public class Example002 {
 	public static void main(String args[]) {
 		System.out.println("println:( 2.00 - 1.10) = " + (2.00 - 1.10));
 		System.out.printf("printf:( 2.00 - 1.10) = %.2f%n", 2.00 - 1.10);
+		System.out.println("BigDecimal:( 2.00 - 1.10) ="
+				+ new BigDecimal("2.00").subtract(new BigDecimal("1.10")));
 	}
 }
