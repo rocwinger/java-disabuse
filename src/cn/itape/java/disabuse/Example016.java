90904ab0f35547d7e7ca6a5475cbf79aa63ec909
imu2008 <panhuawenmail@gmail.com>
2014-12-28 23:05:23 +0800

    http://imu2008.blog.51cto.com/3844842/1597032  【java解惑】Unicode转义字符的使用（续）

---
+++ src/cn/itape/java/disabuse/Example016.java
@@ -0,0 +1,15 @@
+package cn.itape.java.disabuse;
+
+public class Example016 {
+
+	public static void main(String[] args) {
+		char c = 0x000a;
+		/**
+		 * \u000A 注释1
+		 */
+		// Note:\u000A 注释2
+
+		System.out.println("c = " + c);
+		System.out.println("a new line");
+	}
+}
